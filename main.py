7321f90c0538d68596f2567c90e552762d9439a8
pshadoyan@gmail.com <pshadoyan@gmail.com>
2022-06-01 17:27:05 +0000

    Fixes database connection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from secrets import file_
+from secrets import file_, host, dbname, user, passw
 import pandas as pd
 import numpy as np
 import os
@@ -12,9 +12,12 @@ dtype_mappings = {
     'object': 'varchar'
 }
 
-def preproc(file_):
+def pre_proc(file_):
     #read csv into dataframe
-    df = pd.read_csv(file_)
+    try:
+        df = pd.read_csv(file_)
+    except:
+        print("Check CSV file path")
 
     #ensure column formatting is consistent
     df.columns = [x.lower().replace(" ", "").replace("-","_")
@@ -25,9 +28,28 @@ def preproc(file_):
     return df
 
 def create_dbt_format(df, dtype_mappings):
+    #format in a sql-like structure, while replacing the dataframe datatypes into postgres datatypes
     f_columns = ", ".join("{} {}".format(n, d) for (n, d) in zip(df.columns, df.dtypes.replace(dtype_mappings)))
     print(f_columns)
 
+    return f_columns
+
+def db_connection(host, db, user, passw):
+
+    conn = psycopg2.connect(database=db,
+                            user=user,
+                            password=passw,
+                            host=host,
+                            connect_timeout=3)
+
+    print('opened database successfully')
+
+    #returns a cursor to perform database operations
+    return conn.cursor()
+
 if __name__ == "__main__":
-    df = preproc(file_)
-    create_dbt_format(df, dtype_mappings)
+    df = pre_proc(file_)
+    table_f = create_dbt_format(df, dtype_mappings)
+
+    print(host, dbname, user, passw)
+    cursor = db_connection(host, dbname, user, passw)
