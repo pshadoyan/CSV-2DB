b46c90924a6f2bb2a85bcfe394d10235cc4e4081
pshadoyan@gmail.com <pshadoyan@gmail.com>
2022-06-01 18:26:54 +0000

    Adds template and db-connection relevant info

--- secrets.py
+++ secrets.py
@@ -1,1 +1,7 @@
 file_ = '/home/ubuntu/pipeline/CSV-2DB/DATA.csv'
+
+#db connection related information
+host = 'database-2.covvqadnyvtd.us-east-1.rds.amazonaws.com'
+dbname = 'postgres'
+user = 'postgres'
+passw = ''
